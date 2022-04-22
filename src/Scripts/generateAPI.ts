96a1666890ad7caf7ecd929f7a2ab0871347871d
Bogdan Mihaiciuc <bogdanmihaiciuc@gmail.com>
2022-04-22 11:35:05 +0300

    Rename to generateAPI, rename exposed to exported

--- src/Scripts/generateAPI.ts
+++ src/Scripts/generateAPI.ts
@@ -3,19 +3,19 @@ import * as fs from 'fs';
 import { TSUtilities } from '../Utilities/TSUtilities';
 
 /**
- * Builds the thingworx collection declarations of the thingworx project.
+ * Creates a typescript declarations file for the exported types, to be consumed in a frontend or node project.
  */
-export function exposedApi() {
+export function generateAPI() {
 
     const cwd = process.cwd();
-    // Load the twconfig file which contains complication options.
+    // Load the twconfig file which contains compilation options.
     const twConfig = require(`${cwd}/twconfig.json`) as TWConfig;
 
     process.stdout.write(`\x1b[1;31m✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖\x1b[0m\n`);
-    process.stdout.write(`\x1b[1;31m✖✖✖✖✖✖✖\x1b[0m Building exposed APIs is considered experimental and subject to change \x1b[1;31m✖✖✖✖✖✖✖\x1b[0m\n`);
+    process.stdout.write(`\x1b[1;31m✖✖✖✖✖✖✖\x1b[0m Building exported APIs is considered experimental and subject to change \x1b[1;31m✖✖✖✖✖✖✖\x1b[0m\n`);
     process.stdout.write(`\x1b[1;31m✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖✖\x1b[0m\n`);
 
-    process.stdout.write(`\x1b[2m❯\x1b[0m Building exposed API`);
+    process.stdout.write(`\x1b[2m❯\x1b[0m Building exported API`);
     
     // Create a new store for each project
     twConfig.store = {};
@@ -34,12 +34,12 @@ export function exposedApi() {
     for (const key in twConfig.store) {
         if (key == '@globalBlocks') continue;
         const entity = twConfig.store[key];
-        declarations += `\n${entity.toApiDeclaration()}\n`;
+        declarations += `\n${entity.toAPIDeclaration()}\n`;
     }
 
     // Write the declarations to a .d.ts file
     TSUtilities.ensurePath(`${cwd}/api`, cwd);
     fs.writeFileSync(`${cwd}/api/Generated.d.ts`, declarations);
 
-    process.stdout.write(`\r\x1b[1;32m✔\x1b[0m Built exposed API  \n`);
+    process.stdout.write(`\r\x1b[1;32m✔\x1b[0m Built exported API  \n`);
 }
